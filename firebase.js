6f008d3da7354bf5a58adb5ac1eed6645e48eeb0
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-06-13 12:55:24 +0530

    Created a new Expo app

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDRpfbU0yF3tfYneJG5FOKTt6V45_ZhaMw",
+	authDomain: "signal-clone-590.firebaseapp.com",
+	projectId: "signal-clone-590",
+	storageBucket: "signal-clone-590.appspot.com",
+	messagingSenderId: "819373911666",
+	appId: "1:819373911666:web:f914ff2231efa65ea91ceb",
+	measurementId: "G-0T2DTKGD5W",
+};
